c7a85ae9b4e6008cc955d88cc496d3e26820faf0
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-16 10:09:37 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import HeroSection from "@/app/components/HeroSection";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
