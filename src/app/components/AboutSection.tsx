9f0d810cf5f815a06d4951fdcee882548266ecda
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-17 17:36:44 +0500

    Update AboutSection.tsx

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
     });
   }
   return (
-    <section id="about" className='text-white'>
+    <section id="about" className='text-white container pt-32'>
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         <Image src="/about.jpg" alt="About Me" width={500} height={500}/> 
 <div className='mt-4 md:mt-0 text-left flex flex-col h-full '> 
